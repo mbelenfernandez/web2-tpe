df9a8aed69e8f9fe4080974bd37af2c731b47376
Belen Fernandez <mbelenfernandez9@gmail.com>
2023-10-12 23:18:33 -0300

    Agrego estilos bootstrap y corrijo error en editar letra

--- templates/headerAbm.php
+++ templates/headerAbm.php
@@ -13,28 +13,27 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="">Consola de administración</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Consola de administración</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="abmGenero">ABM género</a>
-                        </li> 
+                            <a class="nav-link" href="abmGenero">ABM género</a>
+                        </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="abmCancion">ABM canción</a>
-                        </li>                         
+                            <a class="nav-link" href="abmCancion">ABM canción</a>
+                        </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="logout">Logout</a>
-                        </li>                          
+                            <a class="nav-link" href="logout">Logout</a>
+                        </li>
                     </ul>
                 </div>
             </div>
-            </nav>
+        </nav>
     </header>
 
-    <!-- inicio main container -->
     <main class="container mt-3">
